dfe2875cca6ccd039723014daf221203b1fe54a9
ktpca2 <kajlevidevries@gmail.com>
2026-09-23 11:33:58 +0200

    button script

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,20 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void ExitButton()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+            Application.Quit();
+#endif
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
